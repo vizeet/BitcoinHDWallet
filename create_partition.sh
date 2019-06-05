0d7cabdfa0506d2e6cd43cf76a27d17c156b6f2d
vizeet <vizeet@gmail.com>
2019-06-05 09:41:16 +0530

    updated create partition file

--- create_partition.sh
+++ create_partition.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# unmount partitions
+for l in $(echo "p"| sudo fdisk $1 | grep "^$1"| awk '{print $1}')
+do
+umount $l
+done
+
 # delete all partitions
-for l in $(echo "p"| sudo fdisk /dev/sdb | grep "^$1"| awk '{print $1}')
+for l in $(echo "p"| sudo fdisk $1 | grep "^$1"| awk '{print $1}')
 do
 (echo d; echo 1; echo w) | sudo fdisk $1
 done
@@ -26,9 +32,14 @@ partprobe
 # create linux partition
 (echo n; echo ; echo ; echo w) | sudo fdisk $1
 
-# Empty Partition
-part=$(echo "p"| sudo fdisk /dev/sdb | grep "^/dev/sdb" | awk '$7== "Empty" {print $1}')
-
-./prepare_iso.sh
+# Copy code iso to Empty Partition
+part=$(echo "p"| sudo fdisk $1 | grep "^$1" | awk '$NF== "Empty" {print $1}')
 
 dd if=disk.iso of=$part
+
+# Mount Data Partition
+part=$(echo "p"| sudo fdisk $1 | grep "^$1" | awk '$NF== "Linux" {print $1}')
+mkdir /media/$USER/data
+mount $part /media/$USER/data
+
+
