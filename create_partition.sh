954e9b19154e6370bfcac25e118f447b6a3198ac
vizeet <vizeet@gmail.com>
2019-06-08 10:57:28 +0530

    fixed create_partition script

--- create_partition.sh
+++ create_partition.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-partprobe
+sudo partprobe $1
 
 # unmount partitions
 for l in $(echo "p"| sudo fdisk $1 | grep "^$1"| awk '{print $1}')
@@ -14,25 +14,27 @@ do
 (echo d; echo 1; echo w) | sudo fdisk $1
 done
 
-partprobe
+sudo partprobe $1
 
 # create extended partition
 (echo n; echo e; echo ; echo ; echo ; echo w) | sudo fdisk $1
 
-partprobe
+sudo partprobe $1
 
 # create 200 MB partition
 (echo n; echo ; echo +200M; echo w) | sudo fdisk $1
 
-partprobe
+sudo partprobe $1
 
 # change type to empty
 (echo t; echo ; echo 0; echo w) | sudo fdisk $1
 
-partprobe
+sudo partprobe $1
 
 # create linux partition
 (echo n; echo ; echo ; echo w) | sudo fdisk $1
+
+sudo partprobe $1
 
 # Copy code iso to Empty Partition
 part=$(echo "p"| sudo fdisk $1 | grep "^$1" | awk '$NF== "Empty" {print $1}')
