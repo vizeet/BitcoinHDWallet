defd72788710280982659fc33e15f79e9f30e0b6
vizeet <vizeet@gmail.com>
2019-06-08 18:55:02 +0530

    modified create partition to work with older fdisk

--- create_partition.sh
+++ create_partition.sh
@@ -11,7 +11,12 @@ done
 # delete all partitions
 for l in $(echo "p"| sudo fdisk $1 | grep "^$1"| awk '{print $1}')
 do
+if [ $(echo "p"| sudo fdisk /dev/sdb | grep "^$1"| awk '{print $1}' | wc -l) == 1 ]
+then
+(echo d; echo w) | sudo fdisk $1
+else
 (echo d; echo 1; echo w) | sudo fdisk $1
+fi
 done
 
 sudo partprobe $1
@@ -22,7 +27,7 @@ sudo partprobe $1
 sudo partprobe $1
 
 # create 200 MB partition
-(echo n; echo ; echo +200M; echo w) | sudo fdisk $1
+(echo n; echo l; echo ; echo +200M; echo w) | sudo fdisk $1
 
 sudo partprobe $1
 
@@ -31,23 +36,28 @@ sudo partprobe $1
 
 sudo partprobe $1
 
-# create linux partition
-(echo n; echo ; echo ; echo w) | sudo fdisk $1
-
-sudo partprobe $1
-
 # Copy code iso to Empty Partition
 part=$(echo "p"| sudo fdisk $1 | grep "^$1" | awk '$NF== "Empty" {print $1}')
 
 sudo dd if=disk.iso of=$part
 
-# Mount Data Partition
-part=$(echo "p"| sudo fdisk $1 | grep "^$1" | awk '$NF== "Linux" {print $1}')
-sudo mkdir /media/$USER/data
-sudo mount $part /media/$USER/data
-
 # Change Partition type from Empty to Linux
 partnum=$(echo "p"| sudo fdisk $1 | grep "^$1" | awk '$NF== "Empty" {print substr($1,9)}')
 (echo t; echo $partnum; echo 83; echo w) | sudo fdisk $1
 
+#sudo partprobe $1
+
+# create linux partition
+(echo n; echo l; echo ; echo ; echo w) | sudo fdisk $1
+
 sudo partprobe $1
+
+# Mount Data Partition
+part=$(echo "p"| sudo fdisk $1 | grep "^$1" | tail -n 1 | awk '$NF== "Linux" {print $1}')
+
+echo part=$part
+
+sudo /sbin/mkfs.ext4 -L data $part
+
+sudo mkdir -p /media/$USER/data
+sudo mount $part /media/$USER/data
